0ae3ad2b0a50d3de2de877a7a64a6c81126dd39d
saul813 <saulsabunyo8@gmail.com>
2026-06-27 19:15:54 +0300

    Add Method to update ticket attributes status, priority_level and additional comments

--- src/AgentCentre.java
+++ src/AgentCentre.java
@@ -7,13 +7,19 @@ class Ticket{
     public String category;
     public Date   creationDate;
     public String issueDescription;
+    public String status;
+    public String priorityLevel;
+    public String additionalComments;
 
-    public Ticket(String customerName, String contact, String category, String issueDescription) {
+    public Ticket(String customerName, String contact, String category, String issueDescription, String status, String priorityLevel, String additionalComments) {
         this.customerName = customerName;
         this.contact = contact;
         this.category = category;
         this.creationDate = new Date(); // Automatically sets current date and time
         this.issueDescription = issueDescription;
+        this.status = status;
+        this.priorityLevel = priorityLevel;
+        this.additionalComments = additionalComments;
     }
 
 //    public Ticket createTicket(String customerName, String contact, String category, String issueDescription){
@@ -39,6 +45,28 @@ class Ticket{
     public String getIssueDescription(){
         return issueDescription;
     }
+    public String getStatus(){
+        return status;
+    }
+    public String getPriorityLevel(){
+        return priorityLevel;
+    }
+    public String getAdditionalComments(){
+        return additionalComments;
+    }
+
+    //Setters so that these attributes can be updated
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    public void setPriorityLevel(String priorityLevel) {
+        this.priorityLevel = priorityLevel;
+    }
+    public void setAdditionalComments(String additionalComments) {
+        this.additionalComments = additionalComments;
+    }
+
+
 
 
     //Method to fetch all the ticket details
@@ -48,7 +76,10 @@ class Ticket{
                 "Contact:                 "+ this.contact      +"\n"+
                 "Category:                "+ this.category     +"\n"+
                 "CreationDate:            "+ this.creationDate +"\n"+
-                "issueDescription:        "+ this.issueDescription+"\n"+
+                "IssueDescription:        "+ this.issueDescription+"\n"+
+                "Status:                  "+ this.status       +"\n"+
+                "Priority_Level:          "+ this.priorityLevel +"\n"+
+                "Additional_Comments:     "+ this.additionalComments +"\n"+
                 "--------------------------";
     }
 
@@ -61,8 +92,8 @@ class CallAgent{
     ArrayList<Ticket> ticketsStorage = new ArrayList<>();
 
     //CallAgent Creates Ticket and saves ticket to the tickets storage
-    public Ticket createTicket(String customerName, String contact, String category, String issueDescription){
-        Ticket freshTicket = new Ticket(customerName,contact,category,issueDescription);
+    public Ticket createTicket(String customerName, String contact, String category, String issueDescription, String status, String priorityLevel, String additionalComments){
+        Ticket freshTicket = new Ticket(customerName,contact,category,issueDescription,status,priorityLevel,additionalComments);
         ticketsStorage.add(freshTicket);
         return freshTicket;
     }
@@ -76,6 +107,7 @@ class CallAgent{
         }
         return null; //Ticket not found
     }
+
     //CallAgent Deletes a ticket by customerName
     public boolean deleteTicketByCustomer(String customerName){
         for(int i = 0; i <= ticketsStorage.size(); i++){
@@ -86,6 +118,25 @@ class CallAgent{
         }
         return false;
     }
+
+    //CallAgent updates a ticket
+    public boolean updateTicket(String customerName, String newStatus, String newPriorityLevel, String newAdditionalComments){
+        //Loop through List to find a particular ticket
+        for(int i = 0; i <= ticketsStorage.size(); i++){
+            if(ticketsStorage.get(i).getCustomerName().equalsIgnoreCase(customerName)){
+                //Target that particular ticket matching the customer name
+                Ticket particularTicket = ticketsStorage.get(i);
+
+                //Apply the updates to that particular ticket
+                particularTicket.setStatus(newStatus);
+                particularTicket.setPriorityLevel(newPriorityLevel);
+                particularTicket.setAdditionalComments(newAdditionalComments);
+
+                return true; //Updates complete
+            }
+        }
+        return false; // Updates failed
+    }
 }
 
 
@@ -95,8 +146,8 @@ public class AgentCentre {
         CallAgent ca = new CallAgent();
 
         //Call Agent creating the tickets
-        ca.createTicket("Alice", "alice@email.com", "Network", "Wi-Fi dropping frequently");
-        ca.createTicket("Bob", "bob@email.com", "Billing", "Overcharged on monthly statement");
+        ca.createTicket("Alice", "alice@email.com", "Network", "Wi-Fi dropping frequently","Open","Medium","No Comments yet");
+        ca.createTicket("Bob", "bob@email.com", "Billing", "Overcharged on monthly statement","Open","Medium","No Comments yet");
 
         //Call Agent Searching for ticket by customer Name
         Ticket ticketfound = ca.getTicketByCustomerName("Alice");
@@ -107,12 +158,19 @@ public class AgentCentre {
         }
 
         //Deleting a ticket
-        boolean isDeleted = ca.deleteTicketByCustomer("Alice");
-        if(isDeleted){
-            System.out.println("Ticket Found and Deleted");
-        }else{
-            System.out.println("Error: Ticket doesn't exist");
-        }
+//        boolean isDeleted = ca.deleteTicketByCustomer("Alice");
+//        if(isDeleted){
+//            System.out.println("Ticket Found and Deleted");
+//        }else{
+//            System.out.println("Error: Ticket doesn't exist");
+//        }
 
+        //Updating ticket
+        boolean isUpdated = ca.updateTicket("Alice","Open","Low","Assigned to a particular developer");
+        if(isUpdated){
+            System.out.println("Ticket updated Successfully");
+        }else {
+            System.out.println("System failed to update Ticket");
+        }
     }
 }
